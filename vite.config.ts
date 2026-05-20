d24bde107c6c4e80a8537a51f8e2d6a3f20ec4bb
Michał Lach <kontakt@michal-lach.pl>
2026-05-20 21:46:27 +0200

    0.1.5

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
         },
       },
     },
